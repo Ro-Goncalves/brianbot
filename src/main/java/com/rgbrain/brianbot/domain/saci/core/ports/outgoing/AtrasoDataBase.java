431313d7b7274e4458f1ce7e8d6115d68bd267e8
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-01-10 17:23:33 -0300

    Refatora interface AtrasoDataBase e adiciona método para obter atrasos por comissionado; inclui consulta para localizar comissionados no repositório AtrasoRepository

--- src/main/java/com/rgbrain/brianbot/domain/saci/core/ports/outgoing/AtrasoDataBase.java
+++ src/main/java/com/rgbrain/brianbot/domain/saci/core/ports/outgoing/AtrasoDataBase.java
@@ -2,12 +2,9 @@ package com.rgbrain.brianbot.domain.saci.core.ports.outgoing;
 
 import java.util.List;
 
-import com.rgbrain.brianbot.domain.saci.core.model.dados.DadosDetalheAtraso;
 import com.rgbrain.brianbot.domain.saci.infrastructure.entity.Atraso;
 
 public interface AtrasoDataBase {
     List<Atraso> obterAtrasos();
-    List<AtrasoComissionado> obterComissionado();
-    List<Atraso> obterAtrasosComissionado(Long idComissionado);
-    DadosDetalheAtraso obterAtrasosDetalhadoComissionado(Long idComissionado);
+    List<Atraso> obterAtrasosPorComissionado(Long idComissionado);
 }
--- src/main/java/com/rgbrain/brianbot/domain/saci/infrastructure/AtrasoRepository.java
+++ src/main/java/com/rgbrain/brianbot/domain/saci/infrastructure/AtrasoRepository.java
@@ -3,11 +3,19 @@ package com.rgbrain.brianbot.domain.saci.infrastructure;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.rgbrain.brianbot.domain.saci.core.model.AtrasoComissionado;
 import com.rgbrain.brianbot.domain.saci.infrastructure.entity.Atraso;
 
 @Repository
 public interface AtrasoRepository extends JpaRepository<Atraso, Long> {
-    List<Atraso> findByIdComissionado(Long idComissionado);    
+    List<Atraso> findByIdComissionado(Long idComissionado);   
+    
+    @Query(value = """
+        SELECT DISTINCT a.idComissionado
+        FROM Atraso a
+    """)
+    List<AtrasoComissionado> localizarComissionados(); 
 }
