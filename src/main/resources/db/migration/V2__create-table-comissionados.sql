6f0c6f93f81355f73b9c211b9376a7f0ac6ec346
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-01-10 17:17:14 -0300

    Adiciona tabelas consorciados, comissionados e atrasos com dados iniciais

---
+++ src/main/resources/db/migration/V2__create-table-comissionados.sql
@@ -0,0 +1,5 @@
+CREATE TABLE comissionados (
+    ID_Comissionado       INTEGER     PRIMARY KEY AUTOINCREMENT,
+    NM_Comissionado       VARCHAR(80) NOT NULL,
+    WhatsApp_Comissionado VARCHAR(13) NOT NULL
+);
