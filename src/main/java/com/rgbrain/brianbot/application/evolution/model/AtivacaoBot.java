066f373a24be7ae082a1af25603b7ae450017118
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-01-23 21:22:00 -0300

    Adiciona enum AtivacaoBot e configurações para integração com OpenAI

---
+++ src/main/java/com/rgbrain/brianbot/application/evolution/model/AtivacaoBot.java
@@ -0,0 +1,15 @@
+package com.rgbrain.brianbot.application.evolution.model;
+
+public enum AtivacaoBot {
+    BRIAN_BOT("/BrianBot");
+
+    private String ativacao;
+
+    AtivacaoBot(String ativacao) {
+        this.ativacao = ativacao;
+    }
+
+    public String getAtivacao() {
+        return ativacao;
+    }
+}
--- src/main/java/com/rgbrain/brianbot/infrastructure/configuracoes/domain/ConfiguracaoBrianBot.java
+++ src/main/java/com/rgbrain/brianbot/infrastructure/configuracoes/domain/ConfiguracaoBrianBot.java
@@ -1,5 +1,8 @@
 package com.rgbrain.brianbot.infrastructure.configuracoes.domain;
 
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,14 +12,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 public class ConfiguracaoBrianBot {
 
+    private static final String BASE_URL = "https://api.groq.com/openai";
+
+	private static final String DEFAULT_MODEL = "llama-3.1-8b-instant";
+    
     @Bean
     RestTemplate restTemplate(){
         return new RestTemplate();
     }
-
+    
     @Bean
     ObjectMapper objectMapper(){
         return new ObjectMapper();
+    }
+
+    @Bean
+    public OpenAiApi chatCompletionApi() {
+        return new OpenAiApi(BASE_URL, System.getenv("GROQ_API_KEY"));
+    }
+
+    @Bean
+    public OpenAiChatModel openAiClient(OpenAiApi openAiApi) {
+        return new OpenAiChatModel(
+            openAiApi, 
+            OpenAiChatOptions.builder()
+                .withModel(DEFAULT_MODEL)
+                .withTemperature(0.85)
+            .build());
     }
     
 }
