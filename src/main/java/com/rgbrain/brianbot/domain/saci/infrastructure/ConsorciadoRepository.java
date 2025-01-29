2f602cec796fc59850d96ed54b6b8a2652b7afe0
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-01-28 23:28:16 -0300

    Remove repositório ConsorciadoRepository, não utilizado no projeto

--- src/main/java/com/rgbrain/brianbot/domain/saci/infrastructure/ConsorciadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rgbrain.brianbot.domain.saci.infrastructure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rgbrain.brianbot.domain.saci.infrastructure.entity.Consorciado;
-
-public interface ConsorciadoRepository extends JpaRepository<Consorciado, Long> {
-    
-}
