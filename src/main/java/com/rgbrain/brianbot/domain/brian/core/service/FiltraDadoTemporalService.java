0bea4adace84935e51c5cf2bda01b1a6027fe702
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-02-24 23:39:13 -0300

    Adiciona serviço para filtrar dados temporais com base na data e horas específicas

---
+++ src/main/java/com/rgbrain/brianbot/domain/brian/core/service/FiltraDadoTemporalService.java
@@ -0,0 +1,35 @@
+package com.rgbrain.brianbot.domain.brian.core.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.function.Predicate;
+
+import com.rgbrain.brianbot.domain.brian.infrastructure.model.DadoTemporal;
+
+public class FiltraDadoTemporalService {
+
+    private static final Integer PRIMEIRA_HORA_MANHA = Integer.valueOf(7);
+    private static final Integer SEGUNDA_HORA_MANHA  = Integer.valueOf(8);
+    private static final Integer PRIMEIRA_HORA_TARDE = Integer.valueOf(18);
+    private static final Integer SEGUNDA_HORA_TARDE  = Integer.valueOf(19);
+
+    public static <T extends DadoTemporal> DadosTemporaisFiltrados filtraDadoTemporal(List<T> dadosTemporais) {
+        var hoje = LocalDate.now();
+
+        var dadosTemporaisFiltrados = dadosTemporais
+            .stream()           
+            .filter(dadoTemporal -> dadoTemporal.getDateToLocaleDateTime().toLocalDate().isEqual(hoje))
+            .filter(
+                filtraPorHora(PRIMEIRA_HORA_MANHA)
+                    .or(filtraPorHora(SEGUNDA_HORA_MANHA))
+                    .or(filtraPorHora(PRIMEIRA_HORA_TARDE))
+                    .or(filtraPorHora(SEGUNDA_HORA_TARDE)))
+            .toList();
+            
+        return new DadosTemporaisFiltrados(dadosTemporaisFiltrados);
+    }
+
+    private static Predicate<DadoTemporal> filtraPorHora(Integer hora) {
+        return dadoTemporal -> dadoTemporal.getDateToLocaleDateTime().getHour() == hora;
+    }
+}
