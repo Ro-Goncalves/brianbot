03224195d90a9414daf14bb7f69481f8cc9f8f5f
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-02-27 23:31:37 -0300

    Adiciona serviço para gerenciamento de candidaturas no sistema LeRecHum

---
+++ src/main/java/com/rgbrain/brianbot/domain/lerechum/core/CandidaturaService.java
@@ -0,0 +1,14 @@
+package com.rgbrain.brianbot.domain.lerechum.core;
+
+import org.springframework.stereotype.Service;
+
+import com.rgbrain.brianbot.domain.lerechum.infrastructure.model.entity.Candidatura;
+
+@Service
+public class CandidaturaService {
+
+    public void salvarCandidatura(Candidatura candidatura) {
+        
+    }
+    
+}
