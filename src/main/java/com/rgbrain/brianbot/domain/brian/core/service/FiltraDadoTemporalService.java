822f5c0b53990e5680f646b807660506b16549ee
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-02-28 14:33:04 -0300

    Altera métodos de filtragem para não serem estáticos na classe FiltraDadoTemporalService

--- src/main/java/com/rgbrain/brianbot/domain/brian/core/service/FiltraDadoTemporalService.java
+++ src/main/java/com/rgbrain/brianbot/domain/brian/core/service/FiltraDadoTemporalService.java
@@ -14,7 +14,7 @@ public class FiltraDadoTemporalService {
     private static final Integer PRIMEIRA_HORA_TARDE = Integer.valueOf(18);
     private static final Integer SEGUNDA_HORA_TARDE  = Integer.valueOf(19);
 
-    public static <T extends ResponseDadoTemporal> DadosTemporaisFiltrados filtraDadoTemporal(List<T> dadosTemporais) {
+    public <T extends ResponseDadoTemporal> DadosTemporaisFiltrados filtraDadoTemporal(List<T> dadosTemporais) {
         var hoje = LocalDate.now();
 
         var dadosTemporaisFiltrados = dadosTemporais
@@ -30,7 +30,7 @@ public class FiltraDadoTemporalService {
         return new DadosTemporaisFiltrados(dadosTemporaisFiltrados);
     }
 
-    private static Predicate<ResponseDadoTemporal> filtraPorHora(Integer hora) {
+    private Predicate<ResponseDadoTemporal> filtraPorHora(Integer hora) {
         return dadoTemporal -> dadoTemporal.getDateToLocaleDateTime().getHour() == hora;
     }
 }
